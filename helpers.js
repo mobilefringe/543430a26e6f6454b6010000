f828ee1f496049c36f61d2dce40dfbda8fe4a178
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-04-15 12:00:09 -0400

    Halifax Centre 2.0  /helpers.js

--- helpers.js
+++ helpers.js
@@ -16,7 +16,40 @@ function show_content(){
     $(".modal-backdrop").remove();
 }
 
-
+function show_cat_stores(){
+    $('.show_cat_stores').click(function(e){
+        var cat_id = $(this).attr('data-id');
+        $('.active_cat').removeClass('active_cat');
+        $(this).addClass('active_cat');
+        var rows = $('.cats_row');
+        rows.hide();
+        $('.store_initial').hide();
+        $('#cat_name_header').text($(this).text());
+        $('#cat_name_header').css('display', 'block');
+        $.each(rows, function(i, val){
+            var cat_array = val.getAttribute('data-cat').split(',');
+            if ($.inArray(cat_id, cat_array) >= 0){
+                $(val).show();
+            }
+        });
+        
+        e.preventDefault();
+    });
+    $('.show_all_stores').click(function(e){
+        $('.active_cat').removeClass('active_cat');
+        $(this).addClass('active_cat');
+        var rows = $('.cats_row');
+        rows.show();
+        $.each($('.store_initial'), function(i, val){
+           if ($(val).text().length > 0){
+               $(val).show();
+           } 
+        });
+        $('#cat_name_header').hide();
+        e.preventDefault();
+    });
+    
+}
 
 function show_pin(param){
 	store_id = $(param).attr('store_id');
