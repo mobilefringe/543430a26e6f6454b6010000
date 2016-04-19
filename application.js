676daaa0f6a7afdbfae3ba0c3dd524fd23bab702
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-04-19 12:19:17 -0400

    Halifax Centre 2.0  /application.js

--- application.js
+++ application.js
@@ -113,3 +113,47 @@ function renderStoreDetails(container, template, collection, slug){
     $(container).html(item_rendered.join(''));
 }
 
+
+function renderPromotions(container, template, collection){
+    var item_list = [];
+    var item_rendered = [];
+    var template_html = $(template).html();
+    Mustache.parse(template_html); 
+    $.each( collection , function( key, val ) {
+        if (val.promotionable_type == "Store") {
+            var store_details = getStoreDetailsByID(val.promotionable_id);
+            val.store_detail_btn = store_details.slug ;
+            val.store_name = store_details.name;
+            val.image_url = val.promo_image_url_abs;
+            val.cat_list = store_details.categories.join(',')
+        }
+        else{
+            val.store_name = mall_name;
+            val.image_url = "http://assets.codecloudapp.com/sites/56c740936e6f642d56000000/image/png/1456246178000/promo_image.png";
+        }
+        
+        if(val.image_url.indexOf('missing.png') > 0){
+            val.image_url  = "http://assets.codecloudapp.com/sites/56c740936e6f642d56000000/image/png/1456246178000/promo_image.png";
+        }
+        
+        if(val.description.length > 200){
+            val.description_short = val.description.substring(0,200) + "...";
+        }
+        else{
+            val.description_short = val.description
+        }
+        
+        var show_date = new Date (val.show_on_web_date + "T05:00:00Z");
+        start = new Date (val.start_date + "T05:00:00Z");
+        end = new Date (val.end_date + "T05:00:00Z");
+    
+        if (start.toDateString() == end.toDateString()) {
+            val.dates = (get_month(start.getMonth()))+" "+(start.getDate());    
+        } else {
+            val.dates = (get_month(start.getMonth()))+" "+(start.getDate())+" - "+get_month(end.getMonth())+" "+end.getDate();    
+        }
+        var rendered = Mustache.render(template_html,val);
+        item_rendered.push(rendered);
+    });
+    $(container).html(item_rendered.join(''));
+}
