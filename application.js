e3e47ee10f550f5447b0928212fbbdbf9ae04490
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-04-19 12:19:25 -0400

    Halifax Centre 2.0  /application.js

--- application.js
+++ application.js
@@ -129,11 +129,11 @@ function renderPromotions(container, template, collection){
         }
         else{
             val.store_name = mall_name;
-            val.image_url = "http://assets.codecloudapp.com/sites/56c740936e6f642d56000000/image/png/1456246178000/promo_image.png";
+            val.image_url = "";
         }
         
         if(val.image_url.indexOf('missing.png') > 0){
-            val.image_url  = "http://assets.codecloudapp.com/sites/56c740936e6f642d56000000/image/png/1456246178000/promo_image.png";
+            val.image_url  = "";
         }
         
         if(val.description.length > 200){
