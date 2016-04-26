369b1db7bdbc1de84ed85749ff0f16020a8b980a
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-04-26 12:34:30 -0400

    Halifax Centre 2.0  /helpers.js

--- helpers.js
+++ helpers.js
@@ -197,3 +197,11 @@ function dateToString(dateString){
   
     return dateFormat.toDateString();
 }
+
+function sortByDate(a, b){
+       
+    var aDate = a.publish_date;
+    var bDate = b.publish_date;
+
+    return ((aDate > bDate) ? -1 : ((aDate < bDate) ? 1 : 0));
+}
