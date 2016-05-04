060e835fe5d8cb60c95c675e33accb308e8f4934
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-05-04 14:47:23 -0400

    Halifax Centre 2.0  /application.js

--- application.js
+++ application.js
@@ -174,6 +174,7 @@ function renderPromoDetails(container, template, collection, centre){
             val.store_name = store_details.name;
             val.store_image = store_details.store_front_url_abs;
             val.store_slug = store_details.slug
+            val.store_show = "display:block";
             if (store_details.website != null && store_details.website.length > 0){
                 val.show = "display:block";
                 val.website = store_details.website
