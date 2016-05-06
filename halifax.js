92495be580c6931765875f64dce70baa54e7949d
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-05-06 12:41:50 -0400

    Halifax Centre 1.1  /halifax.js

--- halifax.js
+++ halifax.js
@@ -447,7 +447,7 @@ function renderStoreWithImgTemplate(template_id,html_id,not_empty_section_id,emp
                     item_list.push(rendered);
                 } 
             }
-            console.log(val)
+            
             else{
                  val.promotionable_name = "Halifax Centre";
                  val.store_img = "http://assets.codecloudapp.com/sites/5438407c6e6f64462d020000/d17d8c90fe9a800df686233fab0e6569/default.jpg";
