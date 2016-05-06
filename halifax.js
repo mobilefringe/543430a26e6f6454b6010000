1812dc8df903c9104c2752812366693f22e73543
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-05-06 11:34:05 -0400

    Halifax Centre 1.1  /halifax.js

--- halifax.js
+++ halifax.js
@@ -309,7 +309,7 @@ function renderStoreWithImgTemplate(template_id,html_id,not_empty_section_id,emp
             }
       }  
       else{
-          
+          val.store_img = changeStoreImgUrl(store_details);
       }
       var rendered = Mustache.render(template_html,val);
       item_list.push(rendered);
