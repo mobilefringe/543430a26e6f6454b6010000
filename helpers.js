4bcc76dfb5c9797e400a5c4e349e8a42f36afc73
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-05-31 15:19:56 -0400

    Halifax Centre 2.0  /helpers.js

--- helpers.js
+++ helpers.js
@@ -49,7 +49,6 @@ function show_content(){
             var hours_day = new Date(v.holiday_date + "T04:00:00Z")
             if(hours_day.setHours(0, 0, 0, 0) == d.setHours(0, 0, 0, 0)){
                 $('#home_hours_container').text("Closed Today")
-                $('.chat_link').hide()
             }
             
         } 
