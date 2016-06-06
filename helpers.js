45fa9bae60c34774cbdd3acb16b108dff36600a7
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-06-06 16:38:01 -0400

    Halifax Centre 2.0  /helpers.js

--- helpers.js
+++ helpers.js
@@ -16,7 +16,7 @@ function init(e){
     
     $('.submenu_expander').click(function(e){
 		e.preventDefault()
-		$('.submenu_expander').slideUp()
+		$('.submenu_expander').next().slideUp()
 		$(this).next().slideToggle();
 		$(this).find('img').toggle();
 	})
