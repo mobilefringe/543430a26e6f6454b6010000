b45c3cce0a6738151a7f4de5bc4d405aed8dc814
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-06-08 11:41:59 -0400

    Halifax Centre 2.0  /application.js

--- application.js
+++ application.js
@@ -632,7 +632,6 @@ function renderPosts(container, template, collection){
         }
         val.description_short = val.description_short.replace("&amp;", "&");
         var date_blog = new Date(val.publish_date);
-        console.log(val)
         val.published_on = get_month(date_blog.getMonth()) + " " + date_blog.getDate() + ", " + date_blog.getFullYear();
         
         var rendered = Mustache.render(template_html,val);
