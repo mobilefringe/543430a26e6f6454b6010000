35bb859ba13e06c9d3811c5d5354818aa16e10f3
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-06-15 11:51:45 -0400

    Halifax Centre 2.0  /application.js

--- application.js
+++ application.js
@@ -606,7 +606,6 @@ function renderHomeHours(container, template, collection){
         } else {
             val.h = "Closed";
         }
-        console.log(val)
         var rendered = Mustache.render(template_html,val);
         item_rendered.push(rendered);
     });
