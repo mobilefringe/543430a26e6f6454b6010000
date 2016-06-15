d0d9aaf760103c85bb7839012631cb179f1515e7
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-06-15 14:27:28 -0400

    Halifax Centre 2.0  /helpers.js

--- helpers.js
+++ helpers.js
@@ -1,6 +1,19 @@
 function init(e){
     $('<div class="modal-backdrop custom_backdrop"><img src="http://kodekloud.s3.amazonaws.com/sites/554a79236e6f64713f000000/69e8cd982124dc73de1f5a67a627ee75/loading.gif" class="" alt=""></div>').appendTo(document.body);
-    
+    $('.open_menu').click(function(e){
+        e.preventDefault();
+        $('body').addClass('no_scroll');
+        $('.mobile_menu_container').addClass('active_menu')
+        console.log("her")
+    });
+    $('#close_menu').click(function(e){
+        e.preventDefault();
+        $('body').removeClass('no_scroll');
+        $('.mobile_menu_container').removeClass('active_menu');
+        $('.sub_menu.hidden_now').css('display', "none")
+        $('.plus_icon').show();
+        $('.minus_icon.menu_icon').hide()
+    });
     
     $('.submenu_expander').click(function(e){
 		e.preventDefault()
@@ -104,20 +117,6 @@ function show_content(){
         $('#stores_container').animate({scrollTop:  scrolled});
     })
     renderHomeHours('#home_hours_container3', '#home_hours_template3', getTodaysHours())
-    $('.open_menu').click(function(e){
-        e.preventDefault();
-        $('body').addClass('no_scroll');
-        $('.mobile_menu_container').addClass('active_menu')
-        console.log("her")
-    });
-    $('#close_menu').click(function(e){
-        e.preventDefault();
-        $('body').removeClass('no_scroll');
-        $('.mobile_menu_container').removeClass('active_menu');
-        $('.sub_menu.hidden_now').css('display', "none")
-        $('.plus_icon').show();
-        $('.minus_icon.menu_icon').hide()
-    });
 }
 
 function show_cat_stores(){
