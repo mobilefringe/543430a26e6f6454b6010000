a9936965bb106ee93a8b9409d5a908dbd5f62d0a
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-06-15 11:52:53 -0400

    Halifax Centre 2.0  /helpers.js

--- helpers.js
+++ helpers.js
@@ -52,7 +52,6 @@ function show_content(){
     var n = d.getDay();
     var today_hours = getTodaysHours();
     renderHomeHours('#home_hours_container', '#home_hours_template', today_hours)
-    console.log(today_hours)
     renderHomeHours('#home_hours_container3', '#home_hours_template3', today_hours)
     $.each( getPropertyHours(), function(i,v){
         if(v.is_holiday == true || v.is_closed == true){
