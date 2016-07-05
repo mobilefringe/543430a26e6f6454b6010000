440e6de28ebc5c483fb9c36e3f4757bbecb4359b
rajbir@mobilefringe.com @ CodeCloud <rajbir@mobilefringe.com @ CodeCloud>
2016-07-05 14:15:50 -0400

    Halifax Centre 2.0  /helpers.js

--- helpers.js
+++ helpers.js
@@ -61,7 +61,7 @@ function show_content(){
         }
         if(v.is_holiday == true && v.is_closed != true){
             var hours_day = new Date(v.holiday_date + "T04:00:00Z")
-            console.log(hours_day)
+            console.log(v.holiday_date)
         }
     })
     var events = getEventsList();
