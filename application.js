07d1354b84ad750f04b64da6c35b6006a75b0d24
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2016-10-20 11:27:35 -0400

    Halifax Centre 2.3  /application.js

--- application.js
+++ application.js
@@ -680,7 +680,7 @@ function renderHomeHours(container, template, collection){
         //     val.h = "Closed";
         // }
         
-        val.day = moment().toString().day(); 
+        val.day = moment().toString(); 
         var d = moment();
         val.month = moment().month();
         val.weekday = moment().date();
