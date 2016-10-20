709e9c8400196394c2123fdb8a641279b8d33f91
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2016-10-20 11:55:41 -0400

    Halifax Centre 2.3  /helpers.js

--- helpers.js
+++ helpers.js
@@ -64,8 +64,8 @@ function show_content(){
             // var hours_day = new Date(v.holiday_date + "T05:00:00Z")
             var hours_day = moment(v.holiday_date).tz(getPropertyTimeZone());
             
-            if(hours_day.setHours(0, 0, 0, 0) == d.setHours(0, 0, 0, 0)){
-            // if (hours_day.format() == d.format()){
+            // if(hours_day.setHours(0, 0, 0, 0) == d.setHours(0, 0, 0, 0)){
+            if (hours_day.format("h:mmA") == d.format("h:mmA")){
                 console.log(v)
                 // var open_time = new Date (v.open_time);
                 // var close_time = new Date (v.close_time);
