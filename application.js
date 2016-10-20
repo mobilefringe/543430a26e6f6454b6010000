733958882152357402051c4bee70c1af1418b389
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2016-10-20 12:41:08 -0400

    Halifax Centre 2.3  /application.js

--- application.js
+++ application.js
@@ -485,9 +485,9 @@ function renderHours(container, template, collection, type){
                 // val.h = val.open_time+ " - " + val.close_time;
                 
                 // var open_time = moment(val.open_time).tz(getPropertyTimeZone()).format();
-                open_time = moment(val.open_time).tz(getPropertyTimeZone());
+                val.open_time = moment(val.open_time).tz(getPropertyTimeZone());
                 // var close_time = moment(val.close_time).tz(getPropertyTimeZone()).format();
-                close_time = moment(val.close_time).tz(getPropertyTimeZone());
+                val.close_time = moment(val.close_time).tz(getPropertyTimeZone());
                 val.h = open_time.format("h:mm A") + " - " + close_time.format("h:mm A");
             } else {
                 "Closed"
