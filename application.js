a8ca7ca2a78c5bb04f8957ee89db3fb85e2bed80
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-03-30 10:04:46 -0400

    Halifax Centre 2.3  /application.js

--- application.js
+++ application.js
@@ -752,7 +752,8 @@ function renderPostDetails(container, template, collection){
         console.log(current_post)
         var blog = getBlogDataBySlug('halifaxcentre-test-blog').posts;
         console.log(blog)
-        // var sorted_blog = blog.sortBy(function(o){ return o.publish_date });
+        var sorted_blog = blog.sortBy(function(o){ return o.publish_date });
+        console.log(sorted_blog)
         // var posts = blog.reverse();
         // var num = 0;
         // $.each(posts, function(i, val){
