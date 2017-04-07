7ef842049813fb53191c234868e3328e85e6c73b
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-04-07 13:52:27 -0400

    Halifax Centre 2.3  /helpers.js

--- helpers.js
+++ helpers.js
@@ -40,12 +40,7 @@ function init(e){
     
     $('#option_selector').change(function(){
         window.location = $(this).val();
-    })
-    $('.modal-backdrop').click(function(){
-            $('.custom_modal').fadeOut();
-            $('.modal-backdrop').remove();
-        });
-
+    });
 }
 
 function show_content(){
