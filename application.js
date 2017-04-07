ebf2457426fbf2427611851352ab8ca332c1af98
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-04-07 14:49:20 -0400

    Halifax Centre 2.3  /application.js

--- application.js
+++ application.js
@@ -758,7 +758,6 @@ function renderPostDetails(container, template, collection, blog_posts){
         var index = blog_list.indexOf(current_slug);
         if(index >= 0 && index < blog_list.length){
           var next_slug = blog_list[index + 1];
-          console.log(next_slug);
             if(next_slug != undefined || next_slug != null){
                 val.next_post = "/blog/" + next_slug;
                 val.next_show = "display: block";
@@ -768,7 +767,6 @@ function renderPostDetails(container, template, collection, blog_posts){
         }
         if(index >= 0 && index < blog_list.length){
             var prev_slug = blog_list[index - 1];
-            console.log(prev_slug);
             if(prev_slug != undefined || prev_slug != null){
                 val.prev_post = "/blog/" + prev_slug;
                 val.prev_show = "display: block";
@@ -776,22 +774,6 @@ function renderPostDetails(container, template, collection, blog_posts){
                 val.prev_show = "display: none";
             }
         }
-            
-        // var prev_slug = getPrevPublishedPostBySlug(val.slug);
-        // if(prev_slug != undefined || prev_slug != null){
-        //     val.prev_post = "/blog/" + prev_slug.slug;
-        //     val.prev_show = "display: block";
-        // } else {
-        //     val.prev_show = "display: none";
-        // }
-        
-        // var next_slug = getNextPublishedPostBySlug(val.slug);
-        // if(next_slug != undefined || next_slug != null){
-        //     val.next_post = "/blog/" + next_slug.slug;
-        //     val.next_show = "display: block";
-        // } else {
-        //     val.next_show = "display: none";
-        // }
 
         val.twitter_title = val.title + " via @shopHSC";
         
