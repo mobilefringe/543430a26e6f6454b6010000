be0521ec44e199a919e4fcc6c00c38c2c1a1070c
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-10-12 21:41:19 -0400

    Halifax Centre 2.4  /application.js

--- application.js
+++ application.js
@@ -239,9 +239,10 @@ function renderEvents(container, template, collection, centre){
             val.store_detail_btn = store_details.slug ;
             val.store_name = store_details.name;
             val.image_url = store_details.store_front_url_abs;
-            val.cat_list = store_details.categories.join(',')
-        }
-        else {
+            if(store_details.categories != null){
+                val.cat_list = store_details.categories.join(',')
+            }
+        } else {
             val.store_name = centre;
             val.image_url = "//codecloud.cdn.speedyrails.net/sites/56c740936e6f642d56000000/image/png/1456246178000/promo_image.png";
         }
@@ -250,8 +251,7 @@ function renderEvents(container, template, collection, centre){
         }
         if (val.description.length > 200){
             val.description_short = val.description.substring(0,200) + "...";
-        }
-        else {
+        } else {
             val.description_short = val.description
         }
         if (val.event_image_url_abs.indexOf('missing.png') > -1){
