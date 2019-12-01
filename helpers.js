f344ac03b2faa091770953c7eace8f67236f5372
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-12-01 13:04:55 -0500

    Halifax Centre 2.4.1  /helpers.js

--- helpers.js
+++ helpers.js
@@ -64,6 +64,7 @@ function show_content(){
             }
         }
         if(v.is_holiday == true){
+            console.log("holiday")
             var hours_day = new Date(v.holiday_date + "T05:00:00Z")
             if(hours_day.setHours(0, 0, 0, 0) == d.setHours(0, 0, 0, 0)){
                 console.log(v)
